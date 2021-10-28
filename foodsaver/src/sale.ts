25a4c39251499111cb155039925a3f39f263b2a7
Enrique <morenocarmonaenrique@gmail.com>
2021-10-28 14:37:04 +0200

    close #21

--- foodsaver/src/sale.ts
+++ foodsaver/src/sale.ts
@@ -1,9 +1,9 @@
-import {Product} from './product.ts';
+import {SpecificProduct} from './specificproduct.ts';
 
 export class Sale {
-    private _id: number;
-    private _userid: number;
-    private _products: [[product: Product, quantity: number]];
+    private _id: number; // ID único asociado a cada venta
+    private _userid: number; // ID del usuario que realiza la venta
+    private _products: [[specificproduct: SpecificProduct, quantity: number]]; // Productos específicos con el número de unidades compradas
 
     constructor(sale?: Sale) {
         if(sale !== undefined) {
@@ -14,7 +14,7 @@ export class Sale {
         else {
             this._id = sale._id;
             this._userid = 0;
-            this._products = [[new Product(), 0]];
+            this._products = [[new SpecificProduct(), 0]];
         }
     }
 
@@ -30,13 +30,5 @@ export class Sale {
 
     public get products() {
         return this._products;
-    }
-
-    public obtainbenefit() {
-        let benefit = 0;
-        this.products.forEach(([p,stock]) => {
-            benefit = p.price * stock;
-        });
-        return benefit;
     }
 }
